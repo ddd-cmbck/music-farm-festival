4a0228b98546c06f4e13cc80b682304f53136c98
Dim4k <dd.darknet.tt@gmail.com>
2024-04-08 14:51:17 +0100

    small changes into functions

--- program/program_loop.py
+++ program/program_loop.py
@@ -1,3 +1,14 @@
+# global variables
+DAY1_PRICE = 850
+DAY2_PRICE = 850
+WEEKEND_CAMP_PRICE = 2000
+DAY1_NUMBER_OF_TICKETS = 50
+DAY2_NUMBER_OF_TICKETS = 50
+WEEKEND_CAMP_NUMBER_OF_TICKETS = 10
+FILE_PATH_TO_SUMMARY = 'music_festival_docs/booking_data/'
+FILE_PATH_TO_CUSTOMER_DETAILS = 'music_festival_docs/customer_details/'
+
+
 def get_answ(prompt: str):
     while True:
         try:
@@ -64,9 +75,19 @@ def print_receipt(name, ticket_type, group_size, dining, total):
     print(f'{"Total cost:":<15}' + f'{total:<10}')
 
 
+def print_summary(*args):
+    print(get_label('Booking Details', '-'))
+    print(f'{"Day1:":<20}' + f'{args[0]:<10}')
+    print(f'{"Day2:":<20}' + f'{args[1]:<10}')
+    print(f'{"Weekend-Camp:":<20}' + f'{args[2]:<10}')
+    print(f'{"Fine Dining Day 1:":<20}' + f'{args[3]:<10}')
+    print(f'{"Fine Dining Day 2:":<20}' + f'{args[4]:<10}')
+
+
 def calc_total(price, dining, group_size):
-    total = price
-    total += (group_size * 20) if dining and price <= 2000 else total
+    total = price * group_size
+    if dining and price <= 2000:
+        total += (group_size * 20)
 
     return total
 
@@ -89,20 +110,21 @@ def get_ticket_type(user_choice):
 
 
 def make_booking():
-    print(get_label('MUSIC FARM FESTIVAL (BOOKING)'))
-    name = get_proper_str('Enter your full name: >> ', end=15)
-    phone_num = get_proper_str('Enter your phone number: >> ', end=10)
+    print(get_label(prog_name='MUSIC FARM FESTIVAL (BOOKING)'))
+    name = get_proper_str(prompt='Enter your full name: >> ', end=15)
+    phone_num = get_proper_str(prompt='Enter your phone number: >> ', end=10)
     print(get_menu(prompt='Choose the ticket type:', menu_items_list=['Day 1', 'Day 2', 'Weekend-Camp']))
-    user_choice = get_pos_int('=>   ', end=3)
+    user_choice = get_pos_int(prompt='=>   ', end=3)
     ticket_type, price = get_ticket_type(user_choice)
-    group_size = get_pos_int('How many people in your group? >> ', end=4)
-    dining = get_answ('Do you require fine dining pass (Y/N)? >> ')
+    group_size = get_pos_int(prompt='How many people in your group? >> ', end=4)
+    dining = get_answ(prompt='Do you require fine dining pass (Y/N)? >> ')
     total = calc_total(price, dining, group_size)
     print_receipt(name, ticket_type, group_size, dining, total)
 
 
 def review_booking():
-    pass
+    print(get_label(prog_name='MUSIC FARM FESTIVAL - SUMMARY'))
+    print_summary()
 
 
 def execute_func(user_choice):
